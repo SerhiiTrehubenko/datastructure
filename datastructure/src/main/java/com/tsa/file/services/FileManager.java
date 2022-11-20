eb60b2f957e2b20abf91f0e06ba306b7ade0de81
Serhii Trehubenko <tsa85.ca@gmail.com>
2022-11-20 10:23:07 +0200

    FileManager - individual review - 1

--- datastructure/src/main/java/com/tsa/file/services/FileManager.java
+++ datastructure/src/main/java/com/tsa/file/services/FileManager.java
@@ -12,25 +12,30 @@ public class FileManager {
     private static final String OPERATION_MOVE = "move";
     private static final String OPERATION_COPY = "copy";
 
-    public static int countFiles(String path){
-        return treeWalkerCounter(getContent(path), FILE);
+    public static int countFiles(String path) {
+        return treeWalkerCounter(getListOfContentByPath(path), FILE);
     }
-    public static int countDirs(String path){
-        return treeWalkerCounter(getContent(path), DIR);
+
+    public static int countDirs(String path) {
+        return treeWalkerCounter(getListOfContentByPath(path), DIR);
     }
-    public static void copy(String from, String to){
+
+    public static void copy(String from, String to) {
         moveOrCopy(from, to, OPERATION_COPY);
     }
 
-    public static void move(String from, String to){
+    public static void move(String from, String to) {
         moveOrCopy(from, to, OPERATION_MOVE);
     }
 
-    public static void delete (String path) {
-        Path pathFrom = Paths.get(path);
-        File file = pathFrom.toFile();
-        if (!file.canExecute()) throw new RuntimeException("There is a problem with the Path");
-        if (file.listFiles() == null || Objects.requireNonNull(file.listFiles()).length == 0) throw new RuntimeException("There is nothing to delete");
+    public static void delete(String path) {
+        File file = Paths.get(path).toFile();
+        if (!file.canExecute()) {
+            throw new RuntimeException("There is a problem with the Path");
+        }
+        if (file.listFiles() == null || Objects.requireNonNull(file.listFiles()).length == 0) {
+            throw new RuntimeException("There is nothing to delete");
+        }
         if (file.isFile()) {
             file.delete();
         } else if (file.isDirectory()) {
@@ -48,76 +53,84 @@ public class FileManager {
                 treeWalkerDeleter(file);
                 file.delete();
             }
-         }
+        }
     }
 
     private static int treeWalkerCounter(File[] files, String type) {
         Objects.requireNonNull(files);
-        int counter = 0;
+        int count = 0;
         for (File file : files) {
             if (file.isDirectory() && file.listFiles() != null) {
-                counter += treeWalkerCounter(file.listFiles(), type);
+                count += treeWalkerCounter(file.listFiles(), type);
             }
             int result = type.equals(FILE) && file.isFile() ? 1 : type.equals(DIR) && file.isDirectory() ? 1 : 0;
-            counter = result + counter;
+            count = result + count;
         }
-        return counter;
+        return count;
     }
 
-    private static File[] getContent(String path) {
+    private static File[] getListOfContentByPath(String path) {
         return Paths.get(path).toFile().listFiles();
     }
-    private static void copyFile (File from, File to, String typeOfOperation) {
-        File newFile = Path.of(to.getAbsolutePath() + "\\" + from.getName()).toFile();
-        if (newFile.exists()) throw new RuntimeException("File is already exist");
-        try (var buffInput = new BufferedInputStream(new FileInputStream(from));
-             var buffOut = new BufferedOutputStream(new FileOutputStream(newFile))) {
-            int counter = 0;
-            int counterForRead;
-            int availableData = buffInput.available();
-            byte[] buffArray = new byte[8*1024];
-            while (counter < availableData) {
-                counterForRead = buffInput.read(buffArray, 0, buffArray.length-1);
-                buffOut.write(buffArray, 0, counterForRead);
-                counter += counterForRead;
-            }
+
+    private static void copyFile(File from, File to, String typeOfOperation) {
+        File newFile = Paths.get(to.getAbsolutePath(), from.getName()).toFile();
+        if (newFile.exists()) {
+            throw new RuntimeException("File is already exist");
+        }
+        try (var input = new FileInputStream(from);
+             var output = new FileOutputStream(newFile)) {
+            input.transferTo(output);
         } catch (Exception e) {
             e.printStackTrace();
         }
-        if (typeOfOperation.equals(OPERATION_MOVE)) from.delete();
+        if (typeOfOperation.equals(OPERATION_MOVE)) {
+            from.delete();
+        }
+
     }
+
     private static void treeWalkerCopier(File fileFrom, File to, String typeOfOperation) {
         for (File file : Objects.requireNonNull(fileFrom.listFiles())) {
             if (file.isFile()) {
                 copyFile(file, to, typeOfOperation);
             }
             if (file.isDirectory()) {
-                File newDir = Path.of(to.getAbsolutePath() + "/" + Path.of(fileFrom.getAbsolutePath()).relativize(Path.of(file.getAbsolutePath()))).toFile();
+                File newDir = Paths.get(to.getAbsolutePath(), Path.of(fileFrom.getAbsolutePath()).relativize(Path.of(file.getAbsolutePath())).toString()).toFile();
                 if (newDir.exists()) {
-                    throw new RuntimeException("The Directory is exist");
+                    throw new RuntimeException("The Directory is already exist");
                 } else {
                     newDir.mkdirs();
                 }
                 treeWalkerCopier(file, newDir, typeOfOperation);
-                if (typeOfOperation.equals(OPERATION_MOVE)) file.delete();
+                if (typeOfOperation.equals(OPERATION_MOVE)) {
+                    file.delete();
+                }
             }
         }
     }
-    private static void moveOrCopy (String from, String to, String typeOfOperation) {
-        Path pathFrom = Paths.get(from);
-        Path pathTo = Paths.get(to);
-        File fileFrom = pathFrom.toFile();
-        File fileTo = pathTo.toFile();
-        if (!fileFrom.canExecute()) throw new RuntimeException("There is a problem with the Path FROM which you want to " + typeOfOperation);
-        if (!fileTo.canExecute()) throw new RuntimeException("There is a problem with the Path TO which you want to " + typeOfOperation);
-        if (fileTo.isFile()) throw new RuntimeException("You cannot " + typeOfOperation + " to file, only to directory");
+
+    private static void moveOrCopy(String from, String to, String typeOfOperation) {
+        File fileFrom = Paths.get(from).toFile();
+        File fileTo = Paths.get(to).toFile();
+        if (!fileFrom.canExecute()) {
+            throw new RuntimeException("There is a problem with the Path FROM which you want to " + typeOfOperation);
+        }
+        if (!fileTo.canExecute()) {
+            throw new RuntimeException("There is a problem with the Path TO which you want to " + typeOfOperation);
+        }
+        if (fileTo.isFile()) {
+            throw new RuntimeException("You cannot " + typeOfOperation + " to file, only to directory");
+        }
         if (fileFrom.isFile()) {
             copyFile(fileFrom, fileTo, typeOfOperation);
-        } else if (fileFrom.isDirectory() && fileFrom.listFiles() !=null) {
-            File newDir = Path.of(fileTo.getAbsolutePath() + "/" + Path.of(fileFrom.getName())).toFile();
+        } else if (fileFrom.isDirectory() && fileFrom.listFiles() != null) {
+            File newDir = Paths.get(fileTo.getAbsolutePath(), fileFrom.getName()).toFile();
             newDir.mkdirs();
             treeWalkerCopier(fileFrom, newDir, typeOfOperation);
-            if (typeOfOperation.equals(OPERATION_MOVE)) fileFrom.delete();
+            if (typeOfOperation.equals(OPERATION_MOVE)) {
+                fileFrom.delete();
+            }
         }
     }
 }
